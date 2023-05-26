73bf14bd8f16011cf13e996e4741c8661a041d62
JuanFakhri <juanfakhri02@gmail.com>
2023-05-26 10:26:36 +0700

    Update 2 Create About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     extend: {
       colors: {
         primary: '#0ea5e9',
+        primary2: '#0EE9BF',
+        primary3: '#0E38E9',
+        secondary: '#6b7280',
         dark: '#0f172a'
       },
       screens: {
