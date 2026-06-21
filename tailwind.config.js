9dbeb606ac7e09259aca3239a1c89df9b907e3cd
Juanfakhri <61308533+JuanFakhri@users.noreply.github.com>
2026-06-21 10:23:40 +0700

    Update container padding in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
   theme: {
     container: {
       center: true,
-      padding: '20px',
+      padding: {
+        DEFAULT: '16px',
+        sm: '20px',
+        md: '24px',
+        lg: '32px',
+      },
     },
     extend: {
       colors: {
